9a840846f119b13d6221757552335b6005d0224f
okqin <jiqin@outlook.com>
2025-04-02 00:14:58 +0800

    refactor: Remove temporary variables, compatible with edition 2024.

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ use newsletter::{telemetry::setup_tracing, HttpServer, Settings};
 async fn main() -> anyhow::Result<()> {
     let conf = Settings::try_load().expect("Failed to read config.");
     setup_tracing(conf.logs.as_ref());
-    HttpServer::try_new(&conf).await?.serve().await
+    let server = HttpServer::try_new(&conf).await?;
+    server.serve().await
 }
